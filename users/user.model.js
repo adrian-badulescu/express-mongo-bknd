322d4ad408df6403bf6593546db3361ae1fc454d
adrianb2104 <adrianb2104@gmail.com>
2019-08-25 13:46:14 +0300

    initial commit

--- users/user.model.js
+++ users/user.model.js
@@ -1,12 +1,15 @@
 const mongoose = require('mongoose');
+const role = require('./../_helpers/role');
 const Schema = mongoose.Schema;
 
 const schema = new Schema({
+    id: { type: Number, unique: true },
     username: { type: String, unique: true, required: true },
     hash: { type: String, required: true },
-    firstName: { type: String, required: true },
-    lastName: { type: String, required: true },
-    createdDate: { type: Date, default: Date.now }
+    email: { type: String, required: true },
+    password: { type: String, required: true },
+    createdDate: { type: Date, default: Date.now },
+    role: { type: role, default: role.User }
 });
 
 schema.set('toJSON', { virtuals: true });
